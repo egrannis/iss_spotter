507f5c6f682372a26a95a266c2aa25a36c6ef64b
Emma Grannis <grannis.emma@gmail.com>
2022-04-15 16:54:22 +0000

    Working on promises portion. Using fetchMyIP and coordsByIP for now.

---
+++ iss_promised.js
@@ -0,0 +1,13 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+const fetchMyIP = function () {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
